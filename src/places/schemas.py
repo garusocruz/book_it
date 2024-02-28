37c70fd3d2487a34c39af657905b262858b0616f
garusocruz <garusotec@gmail.com>
2024-02-28 13:20:28 -0300

    Create schemas of places

---
+++ src/places/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class PlaceBase(BaseModel):
+    capacity: int
+    description: str | None = None
+    is_active: bool
+    name: str
+
+
+class PlaceCreate(PlaceBase):
+    pass
+
+
+class  Place(PlaceBase):
+    id: int
+
+    class Config:
+        from_attributes = True
