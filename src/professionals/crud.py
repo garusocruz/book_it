91fd8bc86ad9f3700644c3275c828b0c899ceb1c
garusocruz <garusotec@gmail.com>
2024-02-29 13:35:44 -0300

    create a professionals CRUD functions

---
+++ src/professionals/crud.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+
+def get_professional(db: Session, professional_id: int):
+    return db.query(models.Professional).filter(models.Professional.id == professional_id).first()
+
+
+def get_professionals(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Professional).offset(skip).limit(limit).all()
+
+
+def create_professioanl(db: Session, professional: schemas.ProfessionalCreate):
+    db_professional = models.Professional(
+        customer_id = professional.customer_id,
+        expertise = professional.expertise,
+        created_at = professional.created_at,
+        updated_at = professional.updated_at)
+    db.add(db_professional)
+    db.commit()
+    db.refresh(db_professional)
+    return db_professional
+
+def update_professional(db:Session, professional_id: int, professional: schemas.ProfessionalCreate):
+    db_professional = db.query(models.Professional).filter(models.Professional.id == professional_id).first()
+    if not db_professional:
+        return None
+    
+    db_professional.expertise = professional.expertise
+    db_professional.updated_at = datetime.now()
+    db.commit()
+    db.refresh(db_professional)
+    return db_professional
+
+def delete_professional(db:Session, professional_id: int):
+    db_professional = db.query(models.Professional).filter(models.Professional.id == professional_id).first()
+    if not db_professional:
+        return None
+    db.delete(db_professional)
+    db.commit()
+    return db_professional
