48c7e1d62b6a1e894971d66ea0a76c39c622c2b8
garusocruz <garusotec@gmail.com>
2024-02-29 17:16:58 -0300

    change field name and add date

--- src/schedules/models.py
+++ src/schedules/models.py
@@ -1,13 +1,11 @@
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, Date, DateTime, ForeignKey, Integer, String
 
 from ..db.database import Base, BaseModel
-from ..places.models import Place
-
 
 class Schedule(Base, BaseModel):
     __tablename__ = "schedules"
 
-    place_id = Column('place_id', Integer, ForeignKey(Place.id), nullable=False)
-    week_day = Column('dia_semana', Integer, nullable=False)
+    week_day = Column('week_day', Integer, nullable=False) # 0 = Sunday, 1 = Monday, 2 = Tuesday, 3 = Wednesday, 4 = Thursday, 5 = Friday, 6 = Saturday
+    date = Column(Date, nullable=False)
     start_at = Column(DateTime, nullable=False)
     finish_at = Column(DateTime, nullable=False)
