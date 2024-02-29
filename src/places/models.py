ed09fdcaa5f238fa5f79c4d407c13ee9fb1345b0
garusocruz <garusotec@gmail.com>
2024-02-29 17:17:18 -0300

    Reference the right Model

--- src/places/models.py
+++ src/places/models.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 
 from ..db.database import Base, BaseModel
-from ..customers.models import Customer
+from ..professionals.models import Professional
 
 from ..db.database import Base, BaseModel
 class Place(Base, BaseModel):
     __tablename__ = "places"
 
-    owner_id = Column("owner_id",Integer, ForeignKey(Customer.id))
+    owner_id = Column("owner_id",Integer, ForeignKey(Professional.id))
     name = Column(String(100))
     description = Column(String(255))
     capacity = Column(Integer)
