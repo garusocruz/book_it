9a1ab59ddb076c4e6b92904847bee3b9bff08f99
garusocruz <garusotec@gmail.com>
2024-02-29 15:59:41 -0300

    Add relationship

--- src/places/models.py
+++ src/places/models.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
 
 from ..db.database import Base, BaseModel
-
+from ..customers.models import Customer
 
 from ..db.database import Base, BaseModel
 class Place(Base, BaseModel):
     __tablename__ = "places"
 
+    owner_id = Column("owner_id",Integer, ForeignKey(Customer.id))
     name = Column(String(100))
     description = Column(String(255))
     capacity = Column(Integer)
