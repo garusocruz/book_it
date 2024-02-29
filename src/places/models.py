82d91acdfde236e20edc3a7850ee37847d2fd0d4
garusocruz <garusotec@gmail.com>
2024-02-29 13:10:46 -0300

    removing id cause now the field is a common

--- src/places/models.py
+++ src/places/models.py
@@ -7,7 +7,6 @@ from ..db.database import Base, BaseModel
 class Place(Base, BaseModel):
     __tablename__ = "places"
 
-    id = Column(Integer, primary_key=True)
     name = Column(String(100))
     description = Column(String(255))
     capacity = Column(Integer)
--- src/schedules/models.py
+++ src/schedules/models.py
@@ -7,7 +7,6 @@ from ..places.models import Place
 class Schedule(Base, BaseModel):
     __tablename__ = "schedules"
 
-    id = Column(Integer, primary_key=True)
     place_id = Column('place_id', Integer, ForeignKey(Place.id), nullable=False)
     week_day = Column('dia_semana', Integer, nullable=False)
     start_at = Column(DateTime, nullable=False)
