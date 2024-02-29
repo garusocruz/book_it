0071f4933c4fce9341b0a86249ee48d7ab6b48c1
garusocruz <garusotec@gmail.com>
2024-02-29 13:09:51 -0300

    Add field id as common fields

--- src/db/database.py
+++ src/db/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, DateTime
+from sqlalchemy import create_engine, Column, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,5 +14,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 class BaseModel():
+    id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=True)
