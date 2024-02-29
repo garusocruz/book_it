0668ac4d7baeb548e0b702acd920bda737b44778
garusocruz <garusotec@gmail.com>
2024-02-29 13:11:54 -0300

    create a customers schemas

---
+++ src/customers/schemas.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+
+class CustomerBase(BaseModel):
+    name: str
+    phone: str
+    cpf: str
+
+
+class CustomerCreate(CustomerBase):
+    created_at: datetime = datetime.now()
+    updated_at: datetime | None = None
+    
+
+class  Customer(CustomerBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime | None = None
+
+    class Config:
+        from_attributes = True
