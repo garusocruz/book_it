75403e4948597530700deef93c840fafd00fe7e9
garusocruz <garusotec@gmail.com>
2024-04-18 20:08:26 -0300

    Add professional_id

--- src/customers/adapters/users.py
+++ src/customers/adapters/users.py
@@ -1,10 +1,12 @@
 from sqlalchemy.orm import Session
 from ...customers import models as customer_models, schemas as customer_schemas
 from ...users import crud as user_crud
+from ...professionals import crud as professional_crud
 
 def get_customer_by_user_id(db: Session, user_id: int):
     customer_data = db.query(customer_models.Customer).filter(customer_models.Customer.user_id == user_id).first()
     user_data = user_crud.get_user(db=db, user_id=user_id)
+    professional_data = professional_crud.get_professional_by_customer_id(db=db, customer_id=customer_data.id)
 
     customer = customer_schemas.CustomerUser(**{
         "name": customer_data.name,
@@ -15,6 +17,7 @@ def get_customer_by_user_id(db: Session, user_id: int):
         "created_at": customer_data.created_at,
         "updated_at": customer_data.updated_at,
         "email": user_data.email,
-        "username": user_data.username
+        "username": user_data.username, 
+        "professional_id": professional_data.id if professional_data else None,
     })
     return customer
